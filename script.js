ed6dfcca44c6aad8db83a596444739f2b251c864
edtrevisanbr <108675177+edtrevisanbr@users.noreply.github.com>
2023-04-07 13:02:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-    document.addEventListener("DOMContentLoaded", function () {
-      const indexClosed = document.getElementById("index-closed-inner");
-      const indexOpened = document.getElementById("index-opened");
+document.addEventListener("DOMContentLoaded", function () {
+  const indexClosed = document.getElementById("index-closed-inner");
+  const indexOpened = document.getElementById("index-opened");
 
-      const openButton = indexClosed.querySelector(".fechar-collapse");
-      const closeButton = indexOpened.querySelector(".fechar-collapse1");
+  const openButton = indexClosed.querySelector(".fechar-collapse");
+  const closeButton = indexOpened.querySelector(".fechar-collapse1");
 
-      openButton.addEventListener("click", function () {
-        indexClosed.closest("#index-closed").style.display = "none";
-        indexOpened.style.display = "block";
-      });
+  openButton.addEventListener("click", function () {
+    indexClosed.closest("#index-closed").classList.add("hidden");
+    indexOpened.classList.remove("hidden");
+  });
 
-      closeButton.addEventListener("click", function () {
-        indexClosed.closest("#index-closed").style.display = "block";
-        indexOpened.style.display = "none";
-      });
-    });
+  closeButton.addEventListener("click", function () {
+    indexClosed.closest("#index-closed").classList.remove("hidden");
+    indexOpened.classList.add("hidden");
+  });
+});
